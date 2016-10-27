1bd0b947a1e3b9de6df7a0ee47d6c1400eadc7f1
Krystyna Malewski <krmalewski@gmail.com>
2016-10-26 23:02:23 -0400

    update dev branch

--- routes/home.js
+++ routes/home.js
@@ -1,4 +1,9 @@
 // create a route handler
 const router = require('express').Router();
 
+// Set up our routes
+router.get('/', (req, res) => {
+  res.render('index');
+});
+
 module.exports = router;
