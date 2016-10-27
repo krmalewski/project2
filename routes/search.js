056433f88f769ab33fe6fd405403bda45917291d
Krystyna Malewski <krmalewski@gmail.com>
2016-10-27 18:58:12 -0400

    Add save to bucketlist function on search page

--- routes/search.js
+++ routes/search.js
@@ -7,8 +7,6 @@ const dbService = require('../models/favorites');
 // Set up our routes
 
 router.get('/', yelpService.getCity, yelpService.searchAttractions, dbService.getFavorites, (req, res) => {
-  const city = res.city;
-  console.log(city)
   res.render('search', {
     favorites: res.favorite || [],
     places: res.search,
@@ -17,23 +15,14 @@ router.get('/', yelpService.getCity, yelpService.searchAttractions, dbService.ge
 });
 
 router.delete('/favorites/:id', yelpService.getCity, dbService.deleteFavorites, (req, res) => {
-  console.log(res.city);
   const city = res.city;
-  res.redirect(`/search?location=${city}`)
+  res.redirect(`/search?location=${city}`);
 });
-//   res.redirect(`/search?location=${city}`);
-// });
 
+router.post('/favorites', yelpService.getCity, dbService.saveFavorite, (req, res) => {
+  const city = res.city;
+  res.redirect(`/search?location=${city}`);
+});
 
-// router.get('/', yelpService.keepCity, yelpService.searchAttractions, dbService.getFavorites, (req, res) => {
-//   const city = res.city;
-//   console.log(city)
-//   res.json(res.search);
-// });
-
-
-// router.delete('/favorites/:id', yelpService.getCity, dbService.getFavorites, dbService.deleteFavorites, (req, res) => {
-//   res.redirect('/search');
-// });
 
 module.exports = router;
