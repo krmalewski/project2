65a4bae0e8a0c30706a2d4e7530a260bcd976887
Krystyna Malewski <krmalewski@gmail.com>
2016-10-28 15:58:26 -0400

    Add geocoding function to initial city search

--- routes/home.js
+++ routes/home.js
@@ -1,16 +1,20 @@
 // create a route handler
 const router = require('express').Router();
 const yelpService = require('../services/yelp');
+const mapService = require('../services/maps');
 
 // Set up our routes
 router.get('/', (req, res) => {
   res.render('index');
 });
 
-
-router.post('/findcity', yelpService.getCity, (req, res) => {
-  const city = res.getcity;
-  res.redirect(`explore?location=${city}`);
+router.get('/findcity', mapService.autocompleteCity, (req, res) => {
+  res.json(res.city);
 });
+
+// router.post('/findcity', yelpService.getCity, (req, res) => {
+//   const city = res.getcity;
+//   res.redirect(`explore?location=${city}`);
+// });
 
 module.exports = router;
--- services/maps.js
+++ services/maps.js
@@ -1,19 +1,23 @@
-const API_URL = 'https://maps.googleapis.com/maps/api/js?';
+const fetch = require('node-fetch');
+
+const API_URL = 'https://maps.googleapis.com/maps/api/geocode/json?';
 const API_KEY = process.env.MAPS_KEY;
 
 
-const AUTO_CITY_URL = 'https://maps.googleapis.com/maps/api/place/autocomplete/json?';
-
-
+https://maps.googleapis.com/maps/api/geocode/json?address=Dublin+Ireland&Ykey=AIzaSyA09kvA9vyvsfp5YwCTMycQ8DUMP5Pzbfo
 
 function autocompleteCity(req, res, next) {
+  const cityString = req.query.location;
+  const array = cityString.split(',');
+  const indexTwo = array[1].trim();
+  console.log(indexTwo)
+  let city = `${array[0]}+${indexTwo}`;
+  console.log(city);
 
-  const URL = `${AUTO_CITY_URL}input=${req.body.location}&types=geocode&key=${API_KEY}`;
-
-  fetch(URL)
+  fetch(`${API_URL}address=${city}&key=${API_KEY}`)
   .then(r => r.json())
   .then((result) => {
-    res.attractions = result.businesses;
+    res.city = result.results;
     next();
   })
   .catch((err) => {
