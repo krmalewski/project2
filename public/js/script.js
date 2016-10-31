405148fd080ec06ced2909ec9aa25f121ec446d8
Krystyna Malewski <krmalewski@gmail.com>
2016-10-31 10:40:57 -0400

    Add weather api to explore page

--- public/js/script.js
+++
@@ -1,9 +0,0 @@
-// console.log('hi');
-
-// (() => {
-//   const array = ['/public/images/travel03.jpg'];
-
-//   $('#index-background').attr('src', array[0]);
-
-//   console.log('js connected')
-// })();
